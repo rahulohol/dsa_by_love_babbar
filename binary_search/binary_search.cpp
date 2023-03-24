31e56a89774cc3814643cb36856003cc67186d0b
Rahul Ohol <rahulohol01@gmail.com>
2023-03-24 11:38:17 +0530

    binary serach

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -5,7 +5,7 @@ int binarySearch(int arr[], int size, int target)
 {
     int start = 0;
     int end = size - 1;
-    int mid = (start + end) / 2;
+    int mid = start + (end - start) / 2;
 
     while (start <= end)
     {
@@ -20,11 +20,11 @@ int binarySearch(int arr[], int size, int target)
         }
         else
         {
-            end = mid - 1;
             // search in left direction
+            end = mid - 1;
         };
         // update middle position
-        mid = (start + end) / 2;
+        mid = start + (end - start) / 2;
     };
     // element is not found
     return -1;
@@ -33,7 +33,7 @@ int binarySearch(int arr[], int size, int target)
 int main()
 {
     int arr[] = {2, 4, 6, 8, 10, 12, 16};
-    int target = 12;
+    int target = 2;
     int size = 7;
 
     int indexOfTarget = binarySearch(arr, size, target);
